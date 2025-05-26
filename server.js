a666ba2aef79846f7aacd930ca8041410e5ea5eb
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-05-26 16:10:03 +0100

    Refactor server.js to improve code organization and readability

--- server.js
+++ server.js
@@ -32,3 +32,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  console.log("Home route")
+})
