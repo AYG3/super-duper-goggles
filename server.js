89950b2bc493d1080ec36aca648f20baad8d8543
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-05-26 16:13:09 +0100

    Fix home route response to return status 201 and message

--- server.js
+++ server.js
@@ -34,5 +34,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
+  
+  return res.status(201).send("Home route")
   console.log("Home route")
 })
