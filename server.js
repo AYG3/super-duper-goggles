9a744077e8b1de6efc563452e72bf69f96ae1f40
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-05-26 16:16:50 +0100

    progress

--- server.js
+++ server.js
@@ -34,7 +34,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get('/', (req, res) => {
-  
   return res.status(201).send("Home route 3000")
-  console.log("Home route")
 })
