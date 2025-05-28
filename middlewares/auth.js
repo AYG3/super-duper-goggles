4d0eada56b3dad37e5f520b8e273ed915f4f9c1e
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-05-28 21:24:12 +0100

    Improve token logging and error message in auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,19 +4,20 @@ import User from "../models/User.js";
 
 const protect = asyncHandler(async (req, res, next) => {
   let token;
-  console.log("Token: ", token);
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       token = req.headers.authorization.split(" ")[1];
+      console.log("Token: ", token)
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = await User.findById(decoded.id).select("-password");
+      console.log("Decoded: ", decoded);
       next();
     } catch (error) {
       res.status(401);
-      throw new Error("Not authorized, token failed");
+      throw new Error("Not authorized, token failed here");
     }
   } else {
     res.status(401);
