2ec2c3d35d3235263c714667732809fb3162b198
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-06-06 14:13:41 +0100

    Refactor CORS configuration in server.js: streamline code by removing commented sections and maintaining existing settings

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import cors from 'cors'
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/users.route.js";
@@ -13,19 +13,10 @@ const app = express();
 // Connect to MongoDB
 connectDB();
 
-// Configure CORS
-// app.use(cors({
-//   origin: "*"
-// }));
-
+//cors
 app.use(cors({
-  // origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // Allow both localhost variations
-  origin: '*', // Allow both localhost variations
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
-
+    origin: '*'
+}))
 
 // Middleware
 app.use(express.json());
@@ -38,13 +29,14 @@ app.use("/api/fields", fieldRoutes);
 
 // Error handling
 app.use((err, req, res, next) => {
-  console.error("Error:", err.message);
   res.status(err.status || 500).json({
     message: err.message || "Server Error",
   });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/', (req, res) => {
+  return res.status(201).send("Home route 3000")
+})
