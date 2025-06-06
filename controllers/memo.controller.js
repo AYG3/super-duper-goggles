8a88020844fbf03a0e7b1e3b043658c6cb07ad57
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-06-06 12:06:37 +0100

    Remove unused imports and commented-out functions from memo.controller.js

--- controllers/memo.controller.js
+++ controllers/memo.controller.js
@@ -1,9 +1,3 @@
-import asyncHandler from "express-async-handler";
-import Memo from "../models/Memo.js";
-import MemoField from "../models/MemoField.js";
-import User from "../models/User.js";
-import { sendEmail } from "../utils/email.js";
-
 // Create and send memo
 const createMemo = asyncHandler(async (req, res) => {
   const { recipients, department, content, title } = req.body;
@@ -63,63 +57,3 @@ const createMemo = asyncHandler(async (req, res) => {
 
   res.status(201).json(memo);
 });
-
-// Get memos for user
-const getMemos = asyncHandler(async (req, res) => {
-  const memos = await Memo.find({
-    $or: [{ recipients: req.user._id }, { department: req.user.department }],
-  }).populate("sender", "name email");
-  res.json(memos);
-});
-
-// Update memo status (e.g., read, acknowledged)
-const updateMemoStatus = asyncHandler(async (req, res) => {
-  const { memoId, status } = req.body;
-  const memo = await Memo.findById(memoId);
-
-  if (!memo) {
-    res.status(404);
-    throw new Error("Memo not found");
-    
-  }
-
-  if (
-    !memo.recipients.includes(req.user._id) &&
-    memo.department !== req.user.department
-  ) {
-    res.status(403);
-    throw new Error("Not authorized to update this memo");
-  }
-
-  memo.status.set(req.user._id.toString(), { status, timestamp: new Date() });
-  memo.updatedAt = new Date();
-  await memo.save();
-
-  res.json(memo);
-});
-
-// Archive memo
-const archiveMemo = asyncHandler(async (req, res) => {
-  const { memoId } = req.params;
-  const memo = await Memo.findById(memoId);
-
-  if (!memo) {
-    res.status(404);
-    throw new Error("Memo not found");
-  }
-
-  if (
-    memo.sender.toString() !== req.user._id.toString() &&
-    req.user.role !== "Admin"
-  ) {
-    res.status(403);
-    throw new Error("Not authorized to archive this memo");
-  }
-
-  memo.status.set("archived", { status: "archived", timestamp: new Date() });
-  await memo.save();
-
-  res.json(memo);
-});
-
-export { createMemo, getMemos, updateMemoStatus, archiveMemo };
