49ec18e17ccd3496715f45f13dba79ffcfa72d07
Ayoku Gilbert <gilbertayoku3@gmail.com>
2025-06-09 12:16:14 +0100

    Refactor CORS configuration in server.js: update settings to allow specific origins and remove redundant code

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ import fieldRoutes from "./routes/fields.route.js";
 dotenv.config();
 const app = express();
 
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
 // Connect to MongoDB
 connectDB();
 
 //cors
-app.use(cors({
-    origin: '*'
-}))
 
 // Middleware
 app.use(express.json());
